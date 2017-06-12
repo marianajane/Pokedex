863783ee26687d608a4b9611e7e06d0bb026ae35
marianajane <mariana.musm@gmail.com>
2017-06-12 15:37:16 -0500

    Desarrollo Modales

--- public/js/app.js
+++ public/js/app.js
@@ -4,24 +4,6 @@ $.getJSON("http://pokeapi.co/api/v2/pokemon/",
 	crearPokemons(pokemons);
 });
 
-// var xhr = new XMLHttpRequest();
-
-// xhr.onreadystatechange = function (e) {
-// 	if (this.readyState === 4) {
-// 		if (this.status === 200) {
-			// var response = JSON.parse(this.response);
-			// var pokemons = response.results;
-			// var squads = JSON.parse(this.response);	
-			// crearPokemons(pokemons);
-// 		}
-		
-// 	}
-// };
-
-// xhr.open("GET", "http://pokeapi.co/api/v2/pokemon/");
-
-// xhr.send();
-
 function crearPokemons(pokemons) {
 	var ul = document.getElementById("pokemons");
 
@@ -33,8 +15,96 @@ function crearPokemons(pokemons) {
 	});
 };
 
+var pokemones= [
+	{
+		"src":"img/bulbasaur.png"
+	},
+	{
+		"src":"img/ivysaur.png"
+	},
+	{
+		"src":"img/venusaur.png"
+	},
+	{
+		"src":"img/charmander.png"
+	},
+	{
+		"src":"img/charmeleon.png"
+	},
+	{
+		"src":"img/charizard.png"
+	},
+	{
+		"src":"img/squirtle.png"
+	},
+	{
+		"src":"img/wartortle.png"
+	},
+	{
+		"src":"img/blastoise.png"
+	},
+	{
+		"src":"img/caterpie.png"
+	},
+	{
+		"src":"img/metapod.png"
+	},
+	{
+		"src":"img/butterfree.png"
+	},
+	{
+		"src":"img/weedle.png"
+	},
+	{
+		"src":"img/kakuna.png"
+	},
+	{
+		"src":"img/beedrill.png"
+	},
+	{
+		"src":"img/pidgey.png"
+	},
+	{
+		"src":"img/pidgeotto.png"
+	},
+	{
+		"src":"img/pidgeot.png"
+	},
+	{
+		"src":"img/rattata.png"
+	},
+	{
+		"src":"img/raticate.png"
+	}
+];
+
+function cargarPagina(){
+  var plantillaPokemons =
+    '<section class="col s3">' +
+      '<a href="#modal1"><img data-pokemon="__data__" class="img-pokemons" src="__imagen__" alt=""></a>'  +
+      '<h5>__nombre__</h5>' +
+    '</section>';
 
 
+ $.getJSON("http://pokeapi.co/api/v2/pokemon/", function(response){
+    var pokemons = response.results;
+    nombrePokemones(pokemons, pokemones);
+  });
+
+  var nombrePokemones = function(pokemons, pokemones){
+    var $contenedor = $("#contenedorPokemons");
+    var plantillaFinal = "";
+    pokemons.forEach(function(pokemon, indice){
+      plantillaFinal += plantillaPokemons.replace("__nombre__", pokemon.name)
+                          .replace("__imagen__", pokemones[indice].src)
+   });
+      $contenedor.html(plantillaFinal);
+  };
+
+   $('.modal').modal();
+};
+
+$(document).ready(cargarPagina);
 
 
 
